3847e18d845722132eaf20e9a5762873f1939e25
Kristoffer Robin Canlas <kristofferrobincanlas@gmail.com>
2020-05-23 21:29:09 +0800

    added dynamic year in a footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -7,13 +7,14 @@ export namespace Footer {
 }
 
 export const Footer: React.FC<Footer.Props> = (props: Footer.Props) => { 
+	const currentYear: number = new Date().getFullYear();
 	return (
 		<footer>
 			<div className={style.hosting}>
 				<a target='_blank' href='https://github.com/robincanlas'><Icon size='huge' name='github' /></a>
 				<a target='_blank' href='https://bitbucket.org/kristofferrobincanlas'><Icon size='huge' name='bitbucket' /></a>
 			</div>
-			<p>© 2020, Designed and Coded by Kristoffer Robin Canlas</p>
+			<p>© {currentYear}, Designed and Coded by Kristoffer Robin Canlas</p>
 		</footer>
 	);
 }
