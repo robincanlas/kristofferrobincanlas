e3a609199b76645d7b667fd48e3ea814eced1add
Kristoffer Robin Canlas <kristofferrobincanlas@Kristoffers-MacBook-Pro.local>
2022-09-03 13:09:07 +0800

    update endpoints

--- src/app/constants/server_url.ts
+++ src/app/constants/server_url.ts
@@ -1,9 +1,9 @@
 import { Models } from 'app/models';
 
 export const endPoint: Models.EndPoint = {
-	photos: 'https://robincanlas-server-typescript.herokuapp.com/photo',
-	work: 'https://robincanlas-server-typescript.herokuapp.com/work',
-	information: 'https://robincanlas-server-typescript.herokuapp.com/information'
+	photos: 'https://robincanlas-server-typescript.onrender.com/photo',
+	work: 'https://robincanlas-server-typescript.onrender.com/work',
+	information: 'https://robincanlas-server-typescript.onrender.com/information'
 };
 
 export const cloudinaryUrl: string = 'https://res.cloudinary.com/speedforce/image/upload/';
