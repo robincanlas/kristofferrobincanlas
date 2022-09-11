170f7672726457a1d2ecfe51e716f5cbc191a97f
kristoffercanlas <kristoffer.canlas@axes.ai>
2022-09-11 21:09:50 +0800

    added node env checker

--- src/app/constants/server_url.ts
+++ src/app/constants/server_url.ts
@@ -1,9 +1,11 @@
 import { Models } from 'app/models';
 
+const baseUrl: string = process.env.NODE_ENV === 'development' ? 'http://localhost:9001' : 'https://robincanlas-server-typescript.onrender.com';
+
 export const endPoint: Models.EndPoint = {
-	photos: 'https://robincanlas-server-typescript.onrender.com/photo',
-	work: 'https://robincanlas-server-typescript.onrender.com/work',
-	information: 'https://robincanlas-server-typescript.onrender.com/information'
+	photos: `${baseUrl}/photo`,
+	work: `${baseUrl}/work`,
+	information: `${baseUrl}/information`
 };
 
 export const cloudinaryUrl: string = 'https://res.cloudinary.com/speedforce/image/upload/';
