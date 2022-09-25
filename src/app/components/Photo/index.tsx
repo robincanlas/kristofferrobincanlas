4cfa06d0a920dcdd0427d6e63fdd70df0b143bcf
kristoffercanlas <kristoffer.canlas@axes.ai>
2022-09-25 15:57:58 +0800

    fixed the overlay when selecting a photo

--- src/app/components/Photo/index.tsx
+++ src/app/components/Photo/index.tsx
@@ -1,76 +1,23 @@
 import * as React from 'react';
-import * as style from './style.css';
 import { Models } from 'app/models';
 import { ProgressiveImage } from 'app/components';
-import { Modal } from 'semantic-ui-react';
 
 export namespace Photo {
 	export interface Props {
 		photo: Models.Photo;
 		lastPhotoRef?: (node: any) => void;
-	}
-
-	export interface InitialModalData {
-		open: boolean;
-		data: Models.Photo | null;
+		handlePhotoClick: (photo: Models.Photo) => void;
 	}
 }
 
-const initialModalData: Photo.InitialModalData = {
-	open: false,
-	data: null
-};
-
 export const Photo: React.FC<Photo.Props> = (props: Photo.Props) => {
-	const [modalData, setModalData] = React.useState<Photo.InitialModalData>(initialModalData);
 
 	const openModal = (data: Models.Photo) => {
-		setModalData({
-			open: true,
-			data: data
-		});
+		props.handlePhotoClick(data);
 	};
 
-	const getModal = (): JSX.Element => {
-		return (
-			<Modal
-				basic
-				size='large'
-				centered={true}
-				onClose={() => setModalData(initialModalData)}
-				open={modalData.open}
-				// dimmer='blurring'
-				// closeIcon
-			>
-				<>
-				{modalData.open && 
-					<>
-						<div onClick={() => setModalData(initialModalData)} className={style['modal-bg-img']}>
-						<img src={`${props.photo.raw}_s.jpg`} />
-						</div>
-					</>
-				}
-				<Modal.Content image>
-					<div onClick={() => setModalData(initialModalData)} className={style['modal-image']}>
-						<ProgressiveImage 
-							sizes='(max-width: 800px) 100vw, 800px'
-							preview={`${modalData.data!.raw}_s.jpg`}
-							image={modalData.data!.src}
-							srcSet={`${modalData.data!.src} 200w,
-								${modalData.data!.src} 400w,
-								${modalData.data!.src} 800w
-							`}
-						/>
-					</div>
-				</Modal.Content>
-				</>
-			</Modal>
-		);
-	};
-	
 	return (
 		<React.Fragment>
-			{modalData.open && getModal()}
 			<span className='ttttt' ref={props.lastPhotoRef} onClick={() => openModal(props.photo)}>
 				<ProgressiveImage 
 					sizes='(max-width: 800px) 100vw, 800px'
--- src/app/pages/PhotoPage/index.tsx
+++ src/app/pages/PhotoPage/index.tsx
@@ -8,13 +8,25 @@ import { PhotoState } from 'app/store/photography/state';
 import { RootState } from 'app/store';
 import { Models } from 'app/models';
 import { Loader, Photo } from 'app/components';
+import { Modal } from 'semantic-ui-react';
+import { ProgressiveImage } from 'app/components';
 
 export namespace _PhotoPage {
 	export interface Props {
 		photography: PhotoState;
 		photoActions: PhotoActions;
 	}
+
+	export interface InitialModalData {
+		open: boolean;
+		photo: Models.Photo | null;
+	}
 }
+
+const initialModalData: _PhotoPage.InitialModalData = {
+	open: false,
+	photo: null
+};
 
 const _PhotoPage: React.FC<_PhotoPage.Props> = (props: _PhotoPage.Props) => {
 	let limit: number = 20;
@@ -22,6 +34,8 @@ const _PhotoPage: React.FC<_PhotoPage.Props> = (props: _PhotoPage.Props) => {
 	const [photos, setPhotos] = React.useState<Models.Photo[]>([]);
 	const [maxPageNumber, setMaxPageNumber] = React.useState<number>(0);
 	const [loading, setLoading] = React.useState<boolean>(true);
+	const [modalData, setModalData] = React.useState<_PhotoPage.InitialModalData>(initialModalData);
+
 	const watcher: React.MutableRefObject<IntersectionObserver | undefined> = React.useRef();	
 	
 	React.useEffect(() => {
@@ -54,6 +68,55 @@ const _PhotoPage: React.FC<_PhotoPage.Props> = (props: _PhotoPage.Props) => {
 		setPhotos((oldPhotos) => oldPhotos.concat(newPhotos));
 	};
 
+	const handlePhotoClick = (photo: Models.Photo | null = null) => {
+		setModalData({
+			open: !modalData.open,
+			photo: photo
+		});
+	};
+
+	const getOverlay = (): JSX.Element => {
+		return (
+			<span className={style['custom-modal']} onClick={() => handlePhotoClick()}>
+				<img src={`${modalData.photo?.raw}_s.jpg`} />
+			</span>
+		);
+	};
+
+	const getModal = (): JSX.Element => {
+		return (
+			<Modal
+				basic
+				size='large'
+				centered={true}
+				onClose={() => handlePhotoClick()}
+				open={modalData.open}
+			>
+			<Modal.Content image>
+				<div onClick={() => handlePhotoClick()} className={style['modal-image']}>
+					<ProgressiveImage 
+						sizes='(max-width: 800px) 100vw, 800px'
+						preview={`${modalData.photo!.raw}_s.jpg`}
+						image={modalData.photo!.src}
+						srcSet={`${modalData.photo!.src} 200w,
+							${modalData.photo!.src} 400w,
+							${modalData.photo!.src} 800w
+						`}
+					/>
+				</div>
+			</Modal.Content>
+			</Modal>
+		);
+	};
+
+	React.useEffect(() => {
+		if (modalData.open) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = 'auto';
+		}
+	}, [modalData.open]);
+
 	React.useEffect(() => {
 		setLoading(false);
 	}, [photos]);
@@ -69,17 +132,21 @@ const _PhotoPage: React.FC<_PhotoPage.Props> = (props: _PhotoPage.Props) => {
 		}
 
 		return (
-			<Container id={style.container}>
-				<span className={style['gallery-list']}>
-					{photos.map((element, index) => {
-						if (index + 1 === photos.length) {
-							return <Photo lastPhotoRef={lastPhotoRef} photo={element} key={element.id} />;
-						} else {
-							return <Photo photo={element} key={element.id} />;
-						}
-					})}
-				</span>
-			</Container>
+			<>
+				{modalData.open && getOverlay()}
+				{modalData.open && getModal()}
+				<Container id={style.container}>
+					<span className={style['gallery-list']}>
+						{photos.map((element, index) => {
+							if (index + 1 === photos.length) {
+								return <Photo lastPhotoRef={lastPhotoRef} photo={element} key={element.id} handlePhotoClick={handlePhotoClick} />;
+							} else {
+								return <Photo photo={element} key={element.id} handlePhotoClick={handlePhotoClick} />;
+							}
+						})}
+					</span>
+				</Container>
+			</>
 		);
 	};
 
