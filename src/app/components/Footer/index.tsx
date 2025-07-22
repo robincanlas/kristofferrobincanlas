efb83860e543b500bef85d30d6fca859cb790a8f
Kristoffer Robin Canlas <kristofferrobincanlas@gmail.com>
2025-07-22 15:14:47 +0800

    update copyright year

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -7,14 +7,14 @@ export namespace Footer {
 }
 
 export const Footer: React.FC<Footer.Props> = (props: Footer.Props) => { 
-	const currentYear: number = 2020;
+	const copyrightYear: string = '2020-2025';
 	return (
 		<footer id={style.footer}>
 			<div className={style.hosting}>
 				<a rel='noopener' target='_blank' href='https://github.com/robincanlas'><Icon size='huge' name='github' /></a>
 				<a rel='noopener' target='_blank' href='https://bitbucket.org/kristofferrobincanlas'><Icon size='huge' name='bitbucket' /></a>
 			</div>
-			<p>© {currentYear}, Designed {`&`} Coded by Kristoffer Robin Canlas</p>
+			<p>© {copyrightYear}, Designed {`&`} Coded by Kristoffer Robin Canlas</p>
 		</footer>
 	);
 }
